259c448b665bbf90f27b8069b4273e935b6f4bdd
pdhoward <patrick.howard@hotmail.com>
2017-08-10 14:35:19 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -3,19 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
 import {Provider} from 'react-redux'
 
 // passes in a function which says if the extension for redux is present on the window
 // object, then invoke it
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
-
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
+)
 
 ReactDOM.render(
-  <Provider store={store} >
+  <Provider store={store}>
     <App />
-  </Provider>, document.getElementById('root'))
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
